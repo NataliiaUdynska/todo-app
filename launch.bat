e936115d0964d1f14477ce31fe3fbadc061e5686
Nataliia-Gennadievna <136744472+Nataliia-Gennadievna@users.noreply.github.com>
2025-09-19 15:10:08 +0300

    Final release: Added To-Do App.exe with icon and launch.bat

---
+++ launch.bat
@@ -0,0 +1,43 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCuDJGmW+0g1Kw8UZguAAH+1ArpR6fjr0/6IpkJQXeEwdsHS2bvu
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF25
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCuDJGmW+0g1Kw8UZguAAF+1S5ETqMnr/6SCukh9
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+title 📝 To-Do List App
+color 0a
+echo.
+echo    📝 Запуск To-Do приложения...
+echo    Ожидайте, сервер запускается...
+echo    Когда увидите "Started TodoApp" — откройте http://localhost:8080
+echo.
+java -jar target/todo-app-0.0.1-SNAPSHOT.jar
+pause
